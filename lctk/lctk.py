8908ec2a55beb5b96b33e76c7fa2a06d37b0d3b6
Evan Wright <evan.btbucket@gmail.com>
2020-04-30 19:29:13 -0400

    Fix binaryTree for 0 values

--- lctk/lctk.py
+++ lctk/lctk.py
@@ -79,12 +79,12 @@ def binaryTree(arr):
 	while queue:
 		cur = queue.pop(0)
 		if i < N:
-			if arr[i]:
+			if arr[i] is not None:
 				cur.left = TreeNode(arr[i])
 				queue.append(cur.left)
 			i += 1
 			if i < N:
-				if arr[i]:
+				if arr[i] is not None:
 					cur.right = TreeNode(arr[i])
 					queue.append(cur.right)
 				i += 1
